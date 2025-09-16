e52674198a83ba9c3e3df57473805cee17d675a2
JoaoScheleder <jg.scheleder@hotmail.com>
2025-09-15 22:41:31 -0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#define GLFW_INCLUDE_VULKAN
+#include "includes/glfw3.h"
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include "includes/GLM/glm.hpp"
+#include "includes/GLM/mat4x4.hpp"
+
+#include <stdio.h>
+
+int main() {
+    // Your code here
+    printf("Hello, World!\n");
+    return 0;
+}
